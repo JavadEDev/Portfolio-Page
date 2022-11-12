a5e5f40748e8e87f60ebf4076516a6cc557caf65
Javad-EB <javad.esmatib@Gmail.com>
2022-11-12 19:54:58 +0100

    blur pic

---
+++ script.js
@@ -0,0 +1,24 @@
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("tab-active");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("tab-active");
+}
+
+const navmenu = document.getElementById("navmenu");
+function openmenu() {
+    navmenu.style.right = "0";
+}
+function closemenu() {
+    navmenu.style.right = "-200px";
+
+}
